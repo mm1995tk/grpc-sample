bc74f2f541950b664b1a3bb8f26cef0fb411ee2e
mm1995tk <jimi.woodstock1995@gmail.com>
2022-03-28 23:44:34 +0900

    レスポンスのときにも処理を挟めるようにした

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,9 +2,16 @@ use sample_a::sample_api_a_server::{SampleApiA, SampleApiAServer};
 use sample_a::{RequestA, ResponseA};
 use sample_b::sample_api_b_server::{SampleApiB, SampleApiBServer};
 use sample_b::{RequestB, ResponseB};
-use tonic::{transport::Server, Request, Response, Status};
-use tower::ServiceBuilder;
+use tonic::{
+    body::BoxBody,
+    transport::{NamedService, Server},
+    Request, Response, Status,
+};
+
 use anyhow;
+use hyper::Body;
+use std::task::{Context, Poll};
+use tower::{Layer, Service, ServiceBuilder};
 
 pub mod sample_a {
     tonic::include_proto!("sample_a");
@@ -48,15 +55,19 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
     let addr = "[::1]:50051".parse()?;
     let sample_a = SampleApiAImpl::default();
     let sample_b = SampleApiBImpl::default();
+    let default_layer = ServiceBuilder::new()
+        .layer(MyMiddlewareLayer::default())
+        .layer(tonic::service::interceptor(intercept0));
 
     let svc_a = ServiceBuilder::new()
+        .layer(MyMiddlewareLayer::default())
         .layer(tonic::service::interceptor(intercept1))
         .service(SampleApiAServer::new(sample_a));
 
     let svc_b = SampleApiBServer::new(sample_b);
 
     Server::builder()
-        .layer(tonic::service::interceptor(intercept0))
+        .layer(default_layer)
         .add_service(svc_a)
         .add_service(svc_b)
         .serve(addr)
@@ -77,14 +88,47 @@ fn intercept1(req: Request<()>) -> Result<Request<()>, Status> {
     Ok(req)
 }
 
-// fn intercept2(req: Request<()>) -> Result<Request<()>, Status> {
-//     println!("Intercepting 2");
+#[derive(Debug, Clone, Default)]
+struct MyMiddlewareLayer;
 
-//     Ok(req)
-// }
+impl<S> Layer<S> for MyMiddlewareLayer {
+    type Service = MyMiddleware<S>;
 
-// fn intercept3(req: Request<()>) -> Result<Request<()>, Status> {
-//     println!("Intercepting 3");
+    fn layer(&self, service: S) -> Self::Service {
+        MyMiddleware { inner: service }
+    }
+}
 
-//     Ok(req)
-// }
+#[derive(Debug, Clone)]
+struct MyMiddleware<S> {
+    inner: S,
+}
+
+impl<S> Service<hyper::Request<Body>> for MyMiddleware<S>
+where
+    S: Service<hyper::Request<Body>, Response = hyper::Response<BoxBody>> + Clone + Send + 'static,
+    S::Future: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = futures::future::BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: hyper::Request<Body>) -> Self::Future {
+        let clone = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+        Box::pin(async move {
+            println!("start");
+            let response = inner.call(req).await?;
+            println!("return");
+            Ok(response)
+        })
+    }
+}
+
+impl<S: NamedService> NamedService for MyMiddleware<S> {
+    const NAME: &'static str = S::NAME;
+}
